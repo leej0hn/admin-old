e509d53329848adc49e59a504ac5c8c1c6ad8c5b
LeeJohn <lidx@communet.io>
2018-10-09 15:26:43 +0800

    spring boot 迁移未成功

--- admin-web/src/main/java/io/communet/admin/AdminWebApplication.java
+++ admin-web/src/main/java/io/communet/admin/AdminWebApplication.java
@@ -4,6 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>function:
@@ -12,6 +22,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>Version: 1.0
  */
 @SpringBootApplication
+@EnableWebMvc
+@ImportResource({"classpath:spring-context*.xml"})//导入原先的context初始化文件
 @Slf4j
 public class AdminWebApplication implements CommandLineRunner {
 
@@ -26,5 +38,29 @@ public class AdminWebApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         log.info("{} boot successfully", "admin-web");
     }
+
+
+    @Bean
+    public FilterRegistrationBean shiroFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean(new DelegatingFilterProxy());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("targetFilterLifecycle", "true");
+        registration.setName("shiroFilter");
+        return registration;
+    }
+
+//    @Bean
+//    public ServletRegistrationBean springServlet(){
+//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new DispatcherServlet(), "/");
+//        Map<String, String> initParameters = new HashMap<>();
+//        initParameters.put("contextConfigLocation","classpath*:/spring-mvc*.xml");
+//        servletRegistrationBean.setInitParameters(initParameters);
+//        servletRegistrationBean.setLoadOnStartup(1);
+//        return servletRegistrationBean;
+//    }
+
+
+
+
 
 }
---
+++ admin-web/src/main/java/io/communet/admin/configuration/WebConfig.java
@@ -0,0 +1,29 @@
+package io.communet.admin.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @description:
+ * @author: LeeJohn
+ * @date: 2018/10/9 15:22
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    /**
+     * jsp路径映射
+     * @param
+     */
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return  viewResolver;
+    }
+
+}
